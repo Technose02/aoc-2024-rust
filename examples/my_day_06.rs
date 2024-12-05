a15c51053853274bf2de3170c1f99e6f754709d8
Dr. Niklas Fischer-von Rönn <Niklas.FischervonRoenn@gdv-dl.de>
2024-12-05 17:47:26 +0100

    day06 prepared

---
+++ examples/my_day_06.rs
@@ -0,0 +1,10 @@
+use aoc2024_rust::day06;
+
+// region:      --- input
+const INPUT: &str = r#""#;
+// endregion:   --- input
+
+fn main() {
+    println!("result of part1 for my input: {}", day06::part1(INPUT));
+    println!("result of part2 for my input: {}", day06::part2(INPUT));
+}
