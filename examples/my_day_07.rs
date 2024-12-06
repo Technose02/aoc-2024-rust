be0a8e3290b4c5d7e1f23b440a86819c1d7e62a6
technose02 <technose02@yahoo.de>
2024-12-06 17:15:30 +0100

    prepared day07

---
+++ examples/my_day_07.rs
@@ -0,0 +1,10 @@
+use aoc2024_rust::day07;
+
+// region:      --- input
+const INPUT: &str = r#""#;
+// endregion:   --- input
+
+fn main() {
+    println!("result of part1 for my input: {}", day07::part1(INPUT));
+    println!("result of part2 for my input: {}", day07::part2(INPUT));
+}
