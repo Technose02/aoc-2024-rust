433f70457bfb948c0e260098fb885d934fee8eb2
technose02 <technose02@yahoo.de>
2024-12-08 10:41:21 +0100

    day09 prepared

---
+++ tests/my_day_09.rs
@@ -0,0 +1,18 @@
+#![allow(unused_variables)]
+
+use aoc2024_rust::day09;
+use aoc2024_rust::util::load_data;
+
+#[test]
+#[ignore = "day09 part1 not yet implemented"]
+fn test_part_1() {
+    let (input, output_1, _) = &load_data("input_day_09.txt", "output_day_09.txt");
+    assert_eq!(day09::part1(input), output_1.parse::<usize>().unwrap());
+}
+
+#[test]
+#[ignore = "day09 part1 not yet implemented"]
+fn test_part_2() {
+    let (input, _, output_2) = &load_data("input_day_09.txt", "output_day_09.txt");
+    assert_eq!(day09::part2(input), output_2.parse::<usize>().unwrap());
+}
--- tests/my_day_XY_template.rs
+++ tests/my_day_XY_template.rs
@@ -4,14 +4,14 @@
 use aoc2024_rust::util::load_data;
 
 #[test]
-#[ignore]
+#[ignore = "just a template"]
 fn test_part_1() {
     let (input, output_1, _) = &load_data("input_day_XY.txt", "output_day_XY.txt");
     //assert_eq!(dayXY::part1(input), output_1.parse::<usize>().unwrap());
 }
 
 #[test]
-#[ignore]
+#[ignore = "just a template"]
 fn test_part_2() {
     let (input, _, output_2) = &load_data("input_day_XY.txt", "output_day_XY.txt");
     //assert_eq!(dayXY::part2(input), output_2.parse::<usize>().unwrap());
